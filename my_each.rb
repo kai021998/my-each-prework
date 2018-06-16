4cb21f55088e69bc0c6f5203c114a3921a9ce714
kai021998 <kai.han@balliol.ox.ac.uk>
2018-06-16 22:47:28 +0100

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+collection = [1, 2, 3, 4]
+
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array [i]
+      i = i + 1
+    end
+    array
+  else
+    print "Hey! No block was given!"
+  end
 end
+
+my_each(collection) { |i| }
